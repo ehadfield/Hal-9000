832d7c33b9ea4ab674eced9b0668b882cb690722
Evan Hadfield <hadfield.evan@gmail.com>
2019-04-05 09:46:11 -0700

    add backend business loops and an empty array for results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 function halsResponse(numberInput) {
-  
-}
+  var theArray = [], count 0;
+  if (inNaN(numberInput)) {
+    return theArray;
+  }else {
+    for (var i = 0; i < numberInput; 1++){
+      count ++;
+      theArray.push(count);
+    }
+    for (var i = 0; i < theArray.length; 1++){
+      if (theArray[i] === 1){
+        theArray[i] = "Beep";
+      }else if (theArray[i] === 2){
+        theArray[i] = "Boop";
+      }else if (theArray[i] === 3){
+        theArray[i] = "I'm sorry, Dave. I'm afraid I can't do that";
+      }
+    }return theArray;
+  }
+};
 
 
 
@@ -15,9 +32,9 @@ $(document).ready(function() {
   $("form#halNine").submit(function(event) {
     event.preventDefault();
     var numberInput = parseInt($("input#davesInput").val());
-    var output = halsResponse(numberInput);
+    var outPut = halsResponse(numberInput);
 
-    $("#result").text(output);
+    $("#result").text(outPut);
 
   });
 });
